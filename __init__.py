6f0317a7436b650f6979646570e2da69f5c8e564
Grant Herbert <gdherbert@gmail.com>
2015-06-26 15:49:49 -0700

    Updated init.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
+__author__ = "Grant Herbert <gdherbert@gmail.com>"
 __VERSION__ = '0.5'
 
 from output_msg import output_msg
 from field_utils import get_field_value_set
+from field_utils import pprint_fields
