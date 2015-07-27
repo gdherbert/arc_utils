377884e9b6f7c8a0cb21eca97d4891f558a762ad
Grant Herbert <gdherbert@gmail.com>
2015-07-27 10:34:40 -0700

    refactored geodatabase utils to own module

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 __VERSION__ = '0.5'
 __AUTHOR__ = 'Grant Herbert'
 
-__all__ = ['field_utils', 'output_msg']
+__all__ = ['field_utils', 'gdb_utils', 'output_msg']
 
 import field_utils
+import gdb_utils
 import output_msg
 
--- gdb_utils.py
+++ gdb_utils.py
@@ -1,58 +1,16 @@
 __AUTHOR__ = 'Grant Herbert'
-"""utilities for working with fields"""
-
-def get_field_value_set(inputTable, field, charset='ascii'):
-    """Get a list of unique field values given an input table,
-       a field name string and an optional charset (default='ascii')
-       ascii charset will force encoding with ignore option
-        Returns a set"""
-    try:
-        valueSet = set() # set to hold unique values
-        # use data access search cursor combined with, 'with'
-        with arcpy.da.SearchCursor(inputTable, field) as values:
-            # iterate through all values returned by Search Cursor
-            for value in values:
-                # Add value to set. If the value is not present,
-                # it will be added. If it is present, the set will not
-                # allow duplicates.
-                if value[0] is None:
-                    #Null value
-                    valueSet.add("")
-                else:
-                    if charset != 'ascii':
-                        valueSet.add(value[0])
-                    else:
-                        #if unicode strings are causing problem, try
-                        valueSet.add(value[0].encode('ascii', 'ignore'))
-        # return list of values
-        return valueSet
-
-    except arcpy.ExecuteError:
-        print arcpy.GetMessages(2)
-    except Exception as e:
-        print e.args[0]
-
-
-def pprint_fields(table):
-    """ pretty print table's fields and their properties """
-    def _print(l):
-        print("".join(["{:>12}".format(i) for i in l]))
-
-    atts = ['name', 'aliasName', 'type', 'baseName', 'domain',
-            'editable', 'isNullable', 'length', 'precision',
-            'required', 'scale',]
-    _print(atts)
-
-    for f in arcpy.ListFields(table):
-        _print(["{:>12}".format(getattr(f, i)) for i in atts])
-
+"""utilities for working with geodatabases"""
 
 def gdb_report_fcs(gdb):
     """output a report of all fields in all featureclasses from a geodatabase
-    to the geodtaabse directory or user folder
+    to the geodatabase directory or user folder
     FeatureDataset, FeatureClass, FieldName, FieldAlias, BaseName, DefaultValue,
     FieldType,Required,Editable,isNullable, FieldLength, FieldPrecision,
     FieldScale, FieldDomain"""
+    import arcpy
+    import datetime
+    import os
+
     startTime = datetime.datetime.today()
     startDateString = startTime.strftime('%Y%m%d')
     try:
@@ -102,7 +60,10 @@ def gdb_report_fcs(gdb):
 
 def gdb_export_domains(gdb):
     """output all the domains in a geodatabase
-    to dbf files to the geodatabase directory or user folder"""
+    to dbf files in the geodatabase directory or user folder"""
+    import arcpy
+    import os
+
     try:
         default_env = arcpy.env.workspace
         desc = arcpy.Describe(gdb)
