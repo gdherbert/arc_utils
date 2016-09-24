39001bd88256dcaa0824c00b9cf086f987c8bd27
Grant Herbert <gdherbert@gmail.com>
2016-09-23 17:21:47 -0700

    Revert "update _init_.py to import tableutil"
    This reverts commit 087fc2d3256ee0d7c831a6b76e79f1518fceb84e.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 __VERSION__ = '0.5'
 __AUTHOR__ = 'Grant Herbert'
 
-__all__ = ['fieldutils', 'gdbutils', 'tableutils']
+__all__ = ['fieldutils', 'gdbutils', 'outputmsg']
 
 import fieldutils
 import gdbutils
