0d4dd05ea0d3be33852c9f0d07ab63a4ad067090
Grant Herbert <gdherbert@gmail.com>
2016-10-05 09:42:38 -0700

    add version checking

--- __init__.py
+++ __init__.py
@@ -4,6 +4,11 @@ __AUTHOR__ = 'Grant Herbert'
 
 __all__ = ['fieldutils', 'gdbutils', 'tableutils']
 
+import sys
 from arcutils import fieldutils
 from arcutils import gdbutils
 from arcutils import tableutils
+if sys.version_info.major == 3:
+    from arcutils import aprxutils
+else:
+    from arcutils import mxdutils
