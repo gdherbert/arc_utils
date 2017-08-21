085c01b93d105cd38ebf4b7e6e843ea92db8fe3b
Grant Herbert <gdherbert@gmail.com>
2017-08-21 16:58:53 -0700

    refactor and add field.py functions

--- table.py
+++ table.py
@@ -1,16 +1,15 @@
 from __future__ import print_function, unicode_literals, absolute_import
-"""utilities for working with tables
+"""utilities for working with tables, featureclasses and fields
 """
 
 import arcpy
 from arcutils.output import output_msg
 from arcutils.output import get_valid_output_path
 
+
 class TableObj(object):
     """ provide methods for working with a table/featureclass
-    
     Usage: tbl = arcutils.table.TableObj(path)
-
     attributes:
         path: a string representing an table/featureclass
     """
@@ -84,7 +83,65 @@ def pprint_fields(table):
         _print(["{:>12}".format(getattr(f, i)) for i in atts])
 
 
-def get_field_info(table):
+def get_max_field_value_length(inputTable, field):
+    """Return the length of the maximum value in the field.
+        inputTable {String}:
+            Path or reference to feature class or table.
+
+        field {String}:
+            name of the field to parse
+
+        :return integer
+    """
+    length = 0
+    with arcpy.da.SearchCursor(inputTable, field) as values:
+            for value in values:
+                if len(value[0]) > length:
+                    length = len(value[0])
+    return length
+
+
+def get_field_value_set(inputTable, field, charset='ascii'):
+    """Return a set of unique field values given an input table,
+       a field name string and an optional charset (default='ascii')
+       ascii charset will force encoding with ignore option.
+
+        inputTable {String}:
+            Path or reference to feature class or table.
+
+        field {String}:
+            name of the field to parse
+
+        charset {String}:
+            character set to use (default = 'ascii').
+            Valid values are those in the Python documentation for string encode.
+        :return set of unique values
+       """
+
+    try:
+        value_set = set() # set to hold unique values
+        with arcpy.da.SearchCursor(inputTable, field) as values:
+            for value in values:
+                if value[0] is None:
+                    value_set.add("")
+                elif isinstance(value[0], (str, unicode)):
+                    if charset != 'ascii':
+                        value_set.add(value[0])
+                    else:
+                        # if unicode strings are causing problem, try
+                        value_set.add(value[0].encode('ascii', 'ignore'))
+                else:
+                    value_set.add(value[0])
+
+        return value_set
+
+    except arcpy.ExecuteError:
+        output_msg(arcpy.GetMessages(2))
+    except Exception as e:
+        output_msg(e.args[0])
+
+
+def get_tsv_field_info(table):
     """ Create a tsv output of a table's fields and their properties
 
         inputTable {String}:
@@ -154,7 +211,7 @@ def compare_table_schemas(tbl1, tbl2):
             Path or reference to feature class or table.
     :return array of results (field not found, field same, etc)
     """
-    result_list= []
+    result_arr= []
     field_dict1 = make_field_dict(tbl1)
     field_dict2 = make_field_dict(tbl2)
     for ifield in sorted(list(set(field_dict1.keys()+field_dict2.keys()))):
@@ -162,11 +219,11 @@ def compare_table_schemas(tbl1, tbl2):
         if not (field_dict1.has_key(ifield)):
             the_result = " {0} not found in {1}".format(ifield, tbl1)
             output_msg(the_result)
-            result_list.append(the_result)
+            result_arr.append(the_result)
         elif not (field_dict2.has_key(ifield)):
             the_result = " {0} not found in {1}".format(ifield, tbl2)
             output_msg(the_result)
-            result_list.append(the_result)
+            result_arr.append(the_result)
         else:
             # string comparison of name, type and length
             if field_dict1[ifield] == field_dict2[ifield]:
@@ -180,9 +237,9 @@ def compare_table_schemas(tbl1, tbl2):
 
                 the_result = " {0} {1} {2} {3} does not exactly match {4} {5} {6} {7}".format(tbl1, ifield, field_one_type, field_one_length, tbl2, ifield, field_two_type, field_two_length)
                 output_msg(the_result)
-                result_list.append(the_result)
+                result_arr.append(the_result)
 
-    return result_list
+    return result_arr
 
 
 def report_fields_to_csv_schema(featureclass):
@@ -225,7 +282,7 @@ def report_fields_to_csv_schema(featureclass):
                 for field in fields:
                     output_msg("Writing {}".format(field.name))
                     logFile.write('"{0}","{1}",{2},{3},{4},"{5}",{6},{7},"{8}",{9},{10},"{11}"\n'.format(
-                         field.name,
+                        field.name,
                         type_conversions[field.type],
                         field.precision,
                         field.scale,
@@ -248,11 +305,11 @@ def report_fields_to_csv_schema(featureclass):
         output_msg("Completed")
         arcpy.env.workspace = default_env
 
+
 def convert_csv_schema_to_table(csv_file, table_name):
     # load the csv file
     # assume headers
     # ignore OBJECTID, SHAPE
     fields_to_ignore =["OBJECTID", "FID", "SHAPE", "SHAPE_AREA", "SHAPE.AREA", "SHAPE.STAREA()", "SHAPE_LENGTH", "SHAPE.LEN", "SHAPE.STLENGTH()"]
     # arcpy.AddField_management(in_table, field_name, field_type, {field_precision}, {field_scale}, {field_length}, {field_alias}, {field_is_nullable}, {field_is_required}, {field_domain})
-
     pass
