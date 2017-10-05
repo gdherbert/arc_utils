ec7465a6441fe063904c2cc5518de2242a6721a4
Grant Herbert <gdherbert@gmail.com>
2017-10-04 19:06:51 -0700

    update gdb export fc information, update domain export

--- gdb.py
+++ gdb.py
@@ -7,58 +7,47 @@ import os
 from arcutils.output import output_msg
 from arcutils.output import get_valid_output_path
 
-def report_all_fields(geodatabase):
-    """Create a cvs report of all fields in all featureclasses/tables from a geodatabase
-    to the geodatabase directory or user folder.
-
-    geodatabase {String}:
+def report_all_fc_as_text(geodatabase, output_file=None, sep=r'\t'):
+    """Create a text report of all fields in all featureclasses/tables from a geodatabase
+    to specified output file.
+    :param geodatabase {String}:
         Path or reference to a geodatabase.
+    :param output_file {String}
+        Path or reference to a text file. If not supplied defaults to gdb directory.
+    :param sep {String}
+        seperator value (eg ',' or r'\t'
     """
-    import datetime
-
     gdb = geodatabase
-    start_time = datetime.datetime.today()
-    start_date_string = start_time.strftime('%Y%m%d')
     default_env = arcpy.env.workspace
-
     try:
         desc = arcpy.Describe(gdb)
         arcpy.env.workspace = gdb
-        gdb_name = desc.name.split(".")[0]
+        if not output_file:
+            path = get_valid_output_path(desc.Path)
+            output_file = os.path.join(path, desc.name.split(".")[0] + ".txt")
 
-        report_dir = get_valid_output_path(desc.Path)
-
-        log_file_name = "{}_GDBFCReport_{}.csv".format(gdb_name, start_date_string)
-        log_file_path = os.path.join(report_dir, log_file_name)
-        output_msg("Report file: {0}".format(log_file_path))
+        output_msg("Writing to: {0}".format(output_file))
         # list all datasets
         datasets = arcpy.ListDatasets(feature_type='feature')
         datasets = [''] + datasets if datasets is not None else []
         # write out gdb info, fields etc
-        with open(log_file_path, "w") as logFile:
-            logFile.write("Geodatabase: {0}\n".format(gdb))
-            logFile.write(
-                "Dataset,FeatureClass,FieldName,FieldAlias,BaseName,DefaultValue,FieldType,Required,Editable,isNullable,FieldLength,FieldPrecision,FieldScale,FieldDomain\n")
+        with open(output_file, "w") as logFile:
+            header = ["Dataset","FeatureClass","name","aliasName","baseName","defaultValue","type","required","editable","isNullable","length","precision","scale","domain"]
+            atts = ['name', 'aliasName', 'type', 'baseName', 'domain',
+                    'editable', 'isNullable', 'length', 'precision',
+                    'required', 'scale', ]
+            header_output = sep.join(["{}".format(i) for i in header])
+            logFile.write(header_output + "\n")
             for tbl in arcpy.ListTables():
                 output_msg("Processing Table: {0}".format(tbl))
                 try:
                     fields = arcpy.ListFields(tbl)
+                    str_output = ""
+                    line_start = "{0}{1}{2}{1}".format("", sep, tbl)
                     for field in fields:
-                        logFile.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}\n".format("", tbl,
-                                                                                                             field.name,
-                                                                                                             field.aliasName,
-                                                                                                             field.baseName,
-                                                                                                             field.defaultValue,
-                                                                                                             field.type,
-                                                                                                             field.required,
-                                                                                                             field.editable,
-                                                                                                             field.isNullable,
-                                                                                                             field.length,
-                                                                                                             field.precision,
-                                                                                                             field.scale,
-                                                                                                             field.domain))
-                    # fields listed, add a blank line
-                    logFile.write("\n")
+                        str_output += line_start + sep.join(["{}".format(getattr(field, i)) for i in atts])
+                        str_output += "\n"
+                    logFile.write(str_output)
 
                 except Exception as e:
                     output_msg(str(e.args[0]))
@@ -70,23 +59,12 @@ def report_all_fields(geodatabase):
                     output_msg("Processing Dataset: {0} \ FeatureClass: {1}".format(dataset, fc))
                     try:
                         fields = arcpy.ListFields(fc)
+                        str_output = ""
+                        line_start = "{0}{1}{2}{1}".format(dataset, sep, fc)
                         for field in fields:
-                            logFile.write(
-                                "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}\n".format(dataset, fc,
-                                                                                                       field.name,
-                                                                                                       field.aliasName,
-                                                                                                       field.baseName,
-                                                                                                       field.defaultValue,
-                                                                                                       field.type,
-                                                                                                       field.required,
-                                                                                                       field.editable,
-                                                                                                       field.isNullable,
-                                                                                                       field.length,
-                                                                                                       field.precision,
-                                                                                                       field.scale,
-                                                                                                       field.domain))
-                        # fields listed, add a blank line
-                        logFile.write("\n")
+                            str_output += line_start + sep.join(["{}".format(getattr(field, i)) for i in atts])
+                            str_output += "\n"
+                        logFile.write(str_output)
 
                     except Exception as e:
                         output_msg(str(e.args[0]))
@@ -101,12 +79,13 @@ def report_all_fields(geodatabase):
         output_msg("Completed")
 
 
-def export_all_domains(geodatabase):
+def export_all_domains_as_dbf(geodatabase, output_folder=None):
     """Output all the domains in a geodatabase
     to dbf files in the geodatabase directory or user folder.
-
-    geodatabase {String}:
+    :param geodatabase {String}:
         Path or reference to a geodatabase.
+    :param output_folder {String}
+        optional path to output folder. If not supplied defaults to gdb folder
     """
     gdb = geodatabase
     default_env = arcpy.env.workspace
@@ -114,8 +93,8 @@ def export_all_domains(geodatabase):
         desc = arcpy.Describe(gdb)
         arcpy.env.workspace = gdb
         domains = desc.domains
-
-        report_dir = get_valid_output_path(desc.Path)
+        if not output_folder:
+            output_folder = get_valid_output_path(desc.Path)
 
         for domain in domains:
             # export the domains to tables in the gdb
@@ -124,8 +103,8 @@ def export_all_domains(geodatabase):
                 arcpy.DomainToTable_management(gdb, domain, table,
                                                'field', 'description', '#')
                 # export the table to dbf
-                output_msg('Exporting {0} domain to dbf in {1}'.format(domain, report_dir))
-                arcpy.TableToDBASE_conversion(Input_Table=table, Output_Folder=report_dir)
+                output_msg('Exporting {0} domain to dbf in {1}'.format(domain, output_folder))
+                arcpy.TableToDBASE_conversion(Input_Table=table, Output_Folder=output_folder)
                 # clean up the table
                 arcpy.Delete_management(table)
             except Exception as e:
