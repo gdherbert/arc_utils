554243633f70072c9c15bc6d601fcbea3db6c467
Grant Herbert <gdherbert@gmail.com>
2021-06-24 17:43:18 -0700

    update version and imports for Pro

--- arc_utils/__init__.py
+++ arc_utils/__init__.py
@@ -1,13 +1,15 @@
 """A collection of python utilities for ArcGIS Desktop and ArcGIS Pro
 """
-__version__ = '0.7'
+__version__ = '1.0'
 __author__ = 'Grant Herbert'
 
 __all__ = ['gdb.py', 'table.py']
 
 import sys
 if sys.version_info.major == 3:
-    raise Exception("This version of arc_utils only supports Python 2.7")
+    from arc_utils import gdb
+    from arc_utils import table
+    from arc_utils import aprx
 else:
     from arc_utils import gdb
     from arc_utils import table
